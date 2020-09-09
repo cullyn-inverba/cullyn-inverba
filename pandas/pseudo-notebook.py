36dca766123d1268f424a792204ebfed8fdcbd88
cullyn <cullyn.newman@gmail.com>
2020-09-08 20:23:45 -0700

    added just exercises. Deleted notebook notes, but will keep in future.

--- pandas/pseudo-notebook.py
+++ pandas/pseudo-notebook.py
@@ -3,5 +3,8 @@ import pandas as pd
 import tensorflow as tf3
 
 # %%
-pd.read_csv('data/titanic.csv')
-    
+import tensorflow as tf3
+# %%
+data = pd.read_csv('data/titanic.csv')
+data.head()
+# %%
