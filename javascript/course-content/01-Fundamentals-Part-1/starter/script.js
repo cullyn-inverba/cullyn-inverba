c6f40a99b8eaa9dbe4cfae13ad43e1c660590252
cullyn <cullyn.newman@gmail.com>
2021-07-05 16:08:49 -0700

    javascript course start

---
+++ javascript/course-content/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+// Values and variables
+const country = "United States";
+const continent = "North America";
+const population = 328 * 10 ** 6;
+console.log(country, continent, population);
+console.log("----------");
+
+// Data types
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland, isIsland);
+console.log(typeof population, population);
+console.log(typeof country, country);
+console.log(typeof language, language);
+console.log("----------");
+
+// let, const, and var
+language = "english";
+// country = "Italy"; <- TypeError assignment to const
+
+// Basic operators
