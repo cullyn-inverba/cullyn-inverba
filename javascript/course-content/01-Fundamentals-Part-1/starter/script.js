d4b0bb305eefca73c83a62719e263d47f16ab4cd
cullyn <cullyn.newman@gmail.com>
2021-07-07 17:24:07 -0700

    js / go updates

--- javascript/course-content/01-Fundamentals-Part-1/starter/script.js
+++ javascript/course-content/01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,13 @@
 // Values and variables
+console.log("Values and variables ↓");
 const country = "United States";
 const continent = "North America";
-const population = 328 * 10 ** 6;
+let population = 328 * 10 ** 6;
 console.log(country, continent, population);
 console.log("----------");
 
 // Data types
+console.log("Data types ↓");
 const isIsland = false;
 let language;
 
@@ -20,3 +22,48 @@ language = "english";
 // country = "Italy"; <- TypeError assignment to const
 
 // Basic operators
+console.log("Basic operators ↓");
+console.log((population / 2).toExponential());
+console.log(population + 1);
+let finlandPop = 6 * 10 ** 6;
+console.log(finlandPop >= population);
+let averagePop = 33 * 10 ** 6;
+console.log(averagePop >= population);
+const description = `${country} is in ${continent}, and its ${population.toExponential()} people speak ${language}`;
+console.log(description);
+console.log("----------");
+
+// Coding challenge 1
+console.log("Coding challenge 1 ↓");
+
+function BMI(m, h) {
+  return m / h ** 2;
+}
+
+let [markHeight, markMass] = [1.77, 74];
+let [johnHeight, johnMass] = [1.8, 70];
+
+console.log(BMI(markMass, markHeight));
+console.log(BMI(johnMass, johnHeight));
+
+let [markBMI, johnBMI] = [BMI(markMass, markHeight), BMI(johnMass, johnHeight)];
+
+[markHeight, markMass] = [1.69, 78];
+[johnHeight, johnMass] = [1.95, 92];
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+[markHeight, markMass] = [1.88, 95];
+[johnHeight, johnMass] = [1.75, 85];
+
+markBMI, (johnBMI = BMI(markMass, markHeight)), BMI(johnMass, johnHeight);
+
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+console.log("----------");
+
+// if / else statements
+
+console.log("----------");
