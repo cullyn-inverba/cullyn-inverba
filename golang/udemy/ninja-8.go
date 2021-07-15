de6f3279345ccdee03c583a64ff3ab89a0437b06
cullyn <cullyn.newman@gmail.com>
2021-07-15 15:14:28 -0700

    go progress

---
+++ golang/udemy/ninja-8.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	learn()
+	// hoe1()
+	// hoe2()
+	// hoe3()
+	// hoe4()
+	// hoe5()
+	// hoe6()
+	// hoe7()
+	// hoe8()
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s: %d", p.Name, p.Age)
+}
+
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByName []Person
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
+
+func learn() {
+	p1 := Person{"James", 32}
+	p2 := Person{"Moneypenny", 27}
+	p3 := Person{"Q", 64}
+	p4 := Person{"M", 56}
+
+	people := []Person{p1, p2, p3, p4}
+
+	fmt.Println(people)
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+	sort.Sort(ByName(people))
+	fmt.Println(people)
+}
