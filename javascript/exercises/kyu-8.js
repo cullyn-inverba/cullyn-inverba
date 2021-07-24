fb123fa57fe2794476ae73c6314124fec3c947d7
cullyn <cullyn.newman@gmail.com>
2021-07-23 19:11:31 -0700

    had to do at least a couple exercises today

--- javascript/exercises/kyu-8.js
+++ javascript/exercises/kyu-8.js
@@ -3,3 +3,23 @@
 function findNeedle(haystack) {
   return 'found the needle at position ' + haystack.indexOf('needle');
 }
+
+// Apparently eval is not recommended, using switch instead.
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
+  }
+}
+
+function noSpace(x) {
+  return x.replace(/\s+/g, '');
+}
