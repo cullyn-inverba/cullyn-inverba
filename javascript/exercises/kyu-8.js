1643e9ef00a4ea050217e8a281bfbbfe017017f5
cullyn <cullyn.newman@gmail.com>
2021-07-26 18:18:24 -0700

    js kyu-7 progress

--- javascript/exercises/kyu-8.js
+++ javascript/exercises/kyu-8.js
@@ -23,3 +23,18 @@ function basicOp(operation, value1, value2) {
 function noSpace(x) {
   return x.replace(/\s+/g, '');
 }
+
+function stringReverse(str) {
+  [...str].reverse().join('');
+}
+
+const removeChar = str => str.slice(1, -1);
+
+// getCount Returns number of voewls in a given string (assumes lowercase, a-z only.)
+const getCount = str => (str.match(/[aeiou]/g) || []).length;
+
+// longest returns a sorted string that consists of the longest
+// sequence of distinct characters coming from both strings.
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join('');
+
+const oddOrEven = arr => (arr.reduce((a, b) => a + b, 0) % 1 ? 'even' : 'odd');
