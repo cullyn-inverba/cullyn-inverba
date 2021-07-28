1ebb6fe5323849129ffdf014769e206d9ee51475
cullyn <cullyn.newman@gmail.com>
2021-07-27 19:13:04 -0700

    this one took  a while

--- javascript/exercises/kyu-7.js
+++ javascript/exercises/kyu-7.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const getDivisorsCnt = n => {
+  let count = 0;
+  [...Array(n ** 0.5 + 1).keys()].slice(1).reduce((s, a) => {
+    if (!(n % a) && a) count++;
+  }, 0);
+  return count;
+};
+
+const getDivisorsCnt2 = n =>
+  [...Array((n / 2) ^ 0)].reduce(
+    (pre, _, idx) => (n % ++idx ? pre : ++pre),
+    1
+  );
