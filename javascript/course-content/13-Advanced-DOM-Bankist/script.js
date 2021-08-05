1384d943589d2eea4131b82dcd29b4ce41429db1
cullyn <cullyn.newman@gmail.com>
2021-08-04 18:24:58 -0700

    dom and events section started

--- javascript/course-content/13-Advanced-DOM-Bankist/script.js
+++ javascript/course-content/13-Advanced-DOM-Bankist/script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// // Selecting
+// console.log(`Selecting ↓`);
+
+// console.log(document.documentElement);
+// const allSelections = document.querySelector('.section');
+
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+
+// console.log('----------');
+
+// // Creating and Inserting
+// console.log(`Creating and Inserting ↓`);
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML = `We use cookies for improve functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+
+// header.append(message);
+
+// document.querySelector('.btn--close-cookie').addEventListener('click', e => message.remove());
+
+// console.log('----------');
+
+// // Styles Attrtribues Classes
+// console.log(`Styles Attrtribues Classes ↓`);
+
+// // Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', '#f5af71');
+
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+
+// logo.alt = 'Beautiful minimalist logo';
+// console.log(logo.alt);
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// console.log('----------');
